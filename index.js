39a8773cf45a34f6b8b541ec0e8a46e0af379da1
Adrian Samarripas <asamarripas@gmail.com>
2018-04-28 05:06:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = { foo: 'bar' };
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]: value})
+  return Object.assign({}, object, {[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
@@ -10,7 +10,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObj = Object.assign({}, object)
+  var newObj = Object.assign({}, object);
   delete newObj[key];
   return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
 }
